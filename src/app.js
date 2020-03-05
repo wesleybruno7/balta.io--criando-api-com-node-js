f75838a1621468623beade13a890a393bdc9a5f4
wesleybruno7 <wesleybruno7@terra.com.br>
2020-03-05 10:47:10 -0300

    Atualizando projeto
    Concluido aula: Criando APIs com NodeJs - Aula 16: Criando um Produto
    
    Cadastro de produto (via POST)

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+//conecta ao banco de dados
+mongoose.connect('mongodb+srv://wesley:23111988@mondodb-cluster-hod3u.mongodb.net/nodestr')
+
+//carrega os models
+const Product = require('./models/product');
 
 //carrega as Rotas
 const indexRoute = require('./routes/index-route');
--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,7 +1,23 @@
 'use strict';
 
+const mongoose = require('mongoose');
+const Product = mongoose.model('Product');
+
 exports.post = (req, res, next) => {
-    res.status(201).send(req.body);
+    var product = new Product(req.body);
+    product
+        .save()
+        .then( x => {
+            res.status(201).send({ 
+                message: 'Produto cadastrado com sucesso!'
+            });
+        }).catch( e => {
+            res.status(400).send({ 
+                message: 'Falha ao cadastrar o produto',
+                data: e
+            });
+        });
+    
 };
 
 exports.put = (req, res, next) => {
